7b6f2ad53cfddc12fa92fa1016a3990a5fd7fe6f
pjs <bavuchoko@naver.com>
2025-02-09 22:48:04 +0900

    깃 삭제된 파일 추가

---
+++ app/src/main/java/com/bavuchoko/jsparkgolf/ui/main/user/UserFragment.kt
@@ -0,0 +1,44 @@
+package com.bavuchoko.jsparkgolf.ui.main.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.bavuchoko.jsparkgolf.R
+import com.bavuchoko.jsparkgolf.common.CommonMethod
+import com.bavuchoko.jsparkgolf.ui.main.MainActivity
+import com.bavuchoko.jsparkgolf.ui.utils.AuthUtils
+
+
+class UserFragment : Fragment() {
+
+    lateinit var mainActivity: MainActivity
+    private lateinit var userNameTextView: TextView
+    lateinit var logoutBtn : TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mainActivity = context as MainActivity
+        val view = inflater.inflate(R.layout.fragment_user, container, false)
+
+        logoutBtn = view.findViewById(R.id.log_out_btn)
+        val accessToken = CommonMethod.getToken(mainActivity)
+        val userName = CommonMethod.getValue(requireContext(),"name")
+
+        userNameTextView = view.findViewById(R.id.user_name)
+        userNameTextView.text = userName
+
+
+
+        logoutBtn.setOnClickListener {
+            CommonMethod.clearAllValue(mainActivity)
+            AuthUtils.checkLoginStatus(mainActivity)
+        }
+        return view
+    }
+
+}
