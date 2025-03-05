7daaa398f3f133154ee702358db40d5754bb2c81
pjs <bavuchoko@naver.com>
2025-03-05 20:36:53 +0900

    [feat] 빠른생성 에서 입력한 숫자만큼 이름 입력칸 생성 하고 시작하기 버튼 활성화

--- app/src/main/java/com/bavuchoko/jsparkgolf/ui/game/create/QuickCreateActivity.kt
+++ app/src/main/java/com/bavuchoko/jsparkgolf/ui/game/create/QuickCreateActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
+import android.util.TypedValue
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.view.animation.BounceInterpolator
@@ -26,6 +27,8 @@ class QuickCreateActivity : AppCompatActivity()  {
     private lateinit var btnBack: Button
     private lateinit var companionPanel: TextView
     private lateinit var inputCompanion : EditText
+    private lateinit var tempUserContainer: LinearLayout
+    private lateinit var btnCreate: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +37,11 @@ class QuickCreateActivity : AppCompatActivity()  {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         btnBack = findViewById(R.id.btn_back)
-
+        btnCreate = findViewById(R.id.btn_create)
 
         companionPanel = findViewById(R.id.panel_input_companion)
         inputCompanion = findViewById(R.id.input_companion)
-
+        tempUserContainer = findViewById(R.id.temp_user_container)
 
         companionPanel.scaleX = 0.5f
         companionPanel.scaleY = 0.5f
@@ -85,20 +88,38 @@ class QuickCreateActivity : AppCompatActivity()  {
         if (input.isNotEmpty()) {
             try {
                 val number = input.toInt()
-
+                var players = number
                 if (number > 3) {
                     Toast.makeText(this,"동반자는 최대 3명 입니다.", Toast.LENGTH_SHORT).show()
                     inputCompanion.setText("3")
                     inputCompanion.setSelection(inputCompanion.length())
+                    players = 3
                 }
                 else if (number < 0) {
                     inputCompanion.setText("0")
                     inputCompanion.setSelection(inputCompanion.length())
+                    players = 0
                 }
+                addDynamicEditTexts(players)
             } catch (e: NumberFormatException) {
                 inputCompanion.setText("")
             }
         }
     }
+
+    private fun addDynamicEditTexts(count: Int) {
+        btnCreate.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13f)
+        btnCreate.setTextColor(resources.getColor(R.color.blue))
+        tempUserContainer.removeAllViews()
+
+        val inflater = layoutInflater
+
+        for (i in 1..count) {
+            val editTextView = inflater.inflate(R.layout.temp_user_name, tempUserContainer, false) as EditText
+            editTextView.hint = "동반자 ${i} 이름 입력"
+            tempUserContainer.addView(editTextView)
+        }
+    }
+
 
 }
