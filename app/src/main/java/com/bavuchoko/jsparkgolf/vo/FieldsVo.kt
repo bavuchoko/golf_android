669d34d173da3c472b64460f003897525dc2874e
bavuchoko <bavuchoko@naver.com>
2025-03-13 19:49:34 +0900

    [fix] 필수값 아니면 null 가능

--- app/src/main/java/com/bavuchoko/jsparkgolf/vo/FieldsVo.kt
+++ app/src/main/java/com/bavuchoko/jsparkgolf/vo/FieldsVo.kt
@@ -6,6 +6,6 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class FieldsVo (
     val id : Int,
-    val name: String,
-    val address: String
+    val name: String?,
+    val address: String?
 ): Parcelable
--- app/src/main/java/com/bavuchoko/jsparkgolf/vo/UserVo.kt
+++ app/src/main/java/com/bavuchoko/jsparkgolf/vo/UserVo.kt
@@ -8,6 +8,6 @@ data class UserVo (
     val id: Long,
     val name: String,
     val birth: String,
-    val portrait: String,
+    val portrait: String?,
     val gender: String
 ) : Parcelable
