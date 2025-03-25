a40079544830e5df450087be2aa5e40469be42c6
bavuchoko <bavuchoko@naver.com>
2025-03-25 22:09:26 +0900

    [style] 경기 상세화면에서 탭버튼을 하단으로 이동

--- app/src/main/java/com/bavuchoko/jsparkgolf/ui/game/view/GameViewActivity.kt
+++ app/src/main/java/com/bavuchoko/jsparkgolf/ui/game/view/GameViewActivity.kt
@@ -1,11 +1,13 @@
 package com.bavuchoko.jsparkgolf.ui.game.view
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bavuchoko.jsparkgolf.R
@@ -60,6 +62,12 @@ class GameViewActivity : AppCompatActivity(), ScoringRoundFragment.ScoreUpdateLi
     private fun initializeViews() {
         btnBack = findViewById(R.id.btn_back)
         tabLayout = findViewById(R.id.tab_layout)
+        tabLayout.setTabRippleColorResource(android.R.color.transparent)
+        tabLayout.setSelectedTabIndicatorColor(resources.getColor(android.R.color.black))
+        tabLayout.setTabTextColors(
+            resources.getColor(R.color.deep_gray),  // 기본 텍스트 색상
+            resources.getColor(R.color.app_color)   // 선택된 텍스트 색상
+        )
         val gameRepository = GameRepository(RetrofitFactory.create(this).create(GameApiService::class.java))
         val scoreRepository = ScoreRepository(RetrofitFactory.create(this).create(ScoreService::class.java))
         gameViewModel = ViewModelProvider(this, GameViewModelFactory(gameRepository)).get(GameViewModel::class.java)
@@ -110,12 +118,12 @@ class GameViewActivity : AppCompatActivity(), ScoringRoundFragment.ScoreUpdateLi
         tabLayout.removeAllTabs()
 
         if (isHost) {
-            tabLayout.addTab(tabLayout.newTab().setText("점수 입력"))
-            tabLayout.addTab(tabLayout.newTab().setText("현재 라운드 보기"))
-            tabLayout.addTab(tabLayout.newTab().setText("전체 점수 보기"))
+            tabLayout.addTab(tabLayout.newTab().setText("점수입력"))
+            tabLayout.addTab(tabLayout.newTab().setText("현재점수"))
+            tabLayout.addTab(tabLayout.newTab().setText("전체점수"))
         } else {
-            tabLayout.addTab(tabLayout.newTab().setText("현재 라운드 보기"))
-            tabLayout.addTab(tabLayout.newTab().setText("전체 점수 보기"))
+            tabLayout.addTab(tabLayout.newTab().setText("현재점수"))
+            tabLayout.addTab(tabLayout.newTab().setText("전체점수"))
         }
 
         if (isHost) {
@@ -126,7 +134,9 @@ class GameViewActivity : AppCompatActivity(), ScoringRoundFragment.ScoreUpdateLi
 
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
+
                 tab?.let {
+                    tab.view.setBackgroundColor(ContextCompat.getColor(this@GameViewActivity, android.R.color.transparent))
                     when (it.position) {
                         0 -> {
 
@@ -152,7 +162,12 @@ class GameViewActivity : AppCompatActivity(), ScoringRoundFragment.ScoreUpdateLi
                 }
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    // 선택되지 않은 탭에 대해 배경을 투명하게 설정
+                    tab.view.setBackgroundColor(ContextCompat.getColor(this@GameViewActivity, android.R.color.transparent))
+                }
+            }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
@@ -172,7 +187,7 @@ class GameViewActivity : AppCompatActivity(), ScoringRoundFragment.ScoreUpdateLi
         }
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.game_view_fragment, fragment)
+            .replace(R.id.game_view_container, fragment)
             .commit()
     }
 
